2fa50127f144f3d96dc4214f8633e02316910688
Huy Tran <baohuy82.th@gmail.com>
2023-10-16 12:14:51 -0700

    commit

--- StudentTester.java
+++ StudentTester.java
@@ -1,14 +1,14 @@
 public class StudentTester {
     public static void main(String[] args) {
         // Testing InpersonStudent
-        InpersonStudent inpersonStudent = new InpersonStudent("John", "Doe", Year.SOPHOMORE, "Computer Science", 1000, 50, 4);
+        InpersonStudent inpersonStudent = new InpersonStudent("Cristiano", "Ronaldo", Year.SENIOR, "Computer Science", 1500, 70, 4);
         inpersonStudent.calculateSemesterFee();
         System.out.println(inpersonStudent.toString());
 
         System.out.println();
 
         // Testing OnlineStudent
-        OnlineStudent onlineStudent = new OnlineStudent("Alice", "Smith", Year.JUNIOR, "Engineering", 900, 3, 2);
+        OnlineStudent onlineStudent = new OnlineStudent("David", "Beckham", Year.JUNIOR, "Software Engineering", 1200, 2, 3);
         onlineStudent.calculateSemesterFee();
         System.out.println(onlineStudent.toString());
     }
